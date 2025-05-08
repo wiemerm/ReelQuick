3fc9131284ac2e917fcfb51657c4d4e5bd8abdf6
Megan Wiemer <megan.wiemer@gmail.com>
2025-05-08 12:13:55 -0500

    Create project

---
+++ ReelQuick/ReelQuick/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReelQuick
+//
+//  Created by Megan Wiemer on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReelQuick/ReelQuick/ReelQuickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelQuickApp.swift
+//  ReelQuick
+//
+//  Created by Megan Wiemer on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ReelQuickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
